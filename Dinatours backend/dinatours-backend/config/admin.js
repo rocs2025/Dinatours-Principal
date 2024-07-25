cd6e314f635af2958c950511ec9f4a24d7754171
Rodrigo <roantocampos@gmail.com>
2024-07-24 20:39:49 -0600

    changes

--- Dinatours backend/dinatours-backend/config/admin.js
+++ Dinatours backend/dinatours-backend/config/admin.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'AORI2VzgRF70o3mu0VF1uw=='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'QfZ6/OGyRyLLDfnzWx2a6A=='),
   },
   transfer: {
     token: {
@@ -14,4 +14,5 @@ module.exports = ({ env }) => ({
     nps: env.bool('FLAG_NPS', true),
     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
   },
+  
 });
